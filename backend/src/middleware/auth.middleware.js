63c1d69c369cdeaab87300196aaf846275226bce
pi8878 <simangolwalifwatila99@gmail.com>
2025-05-20 14:57:07 -0500

    funtions added

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -14,8 +14,17 @@ export const protectRoute = async (req, res, next) => {
         if(!decoded) {
             return res.status(401).json({ message: "Unauthorized - Invalid Token" });
         }
+
+        const user = await User.findById(decoded.userId).select("-password");
+        if (!user) {
+            return res.status(404).json({ message: "Unauthorized - User Not Found" });
+        }
+
+        req.user = user;
+        next();
         
     } catch (error) {
-        
+        console.log("Error in auth middleware", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 }
