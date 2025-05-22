dc2df07eeb304162db439035fce03d86709fabca
pi8878 <simangolwalifwatila99@gmail.com>
2025-05-22 14:23:55 -0500

    implemented the updateprofile function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import {generateToken} from "../utils/utils.js";
+import cloudinary from "../utils/cloudinary.js";
 
 
 export const signup = async(req, res) => {
@@ -88,4 +89,25 @@ export const logout = (req, res) => {
 };
 
 
-export const updateProfile = async (req, res) => {}
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic) {
+            return res.status(400).json({message: "Please provide a profile picture"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            profilePic: uploadResponse.secure_url
+        }, {new: true})
+
+        res.status(200).json(updatedUser)
+        
+    } catch (error) {
+        console.log("error in update profile:", error);
+        res.status(500).json({message: "Internal server error"});
+        
+    }
+}
