3bfafdd981fff5233bb2fc9f15c6e889f3cfe8a0
pi8878 <simangolwalifwatila99@gmail.com>
2025-05-23 00:39:53 -0500

    done with message routes for now

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,7 @@
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+import cloudinary from "../utils/cloudinary.js";
+
 
 export const getUserForSidebar = async (req, res) => {
     try {
@@ -9,6 +13,62 @@ export const getUserForSidebar = async (req, res) => {
     } catch (error) {
         console.log("Error in getUserForSidebar controller", error.message);
         res.status(500).json({ message: "Internal server error" });
+        
+    }
+};
+
+// Finds all messages between personal and other respective user clicked on
+
+export const getMessages = async(req, res) => {
+    try {
+        const {id:userToChatId} = req.params
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or:[
+                {senderId:myId, receiverId:userToChatId},
+                {senderId:userToChatId, receiverId:myId}
+            ]
+        })
+
+        res.status(200).json(messages)
+        
+    } catch (error) {
+        console.log("Error in getMessages controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
+        
+    }
+};
+
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image} = req.body;
+        const {id:receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            // upload base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;      
+        }
+
+        const newMessage =  Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        // todo: realtime functionality goes here => socket.io
+
+        res.status(201).json(newMessage)
+        
+    } catch (error) {
+        console.log("Error in sendMessage controller", error.message);
+        res.status(500).json({ error: "Internal server error" });
         
     }
 };
